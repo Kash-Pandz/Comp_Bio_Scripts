e8cce967389c049a8cde60ac1ae36973132d2dc2
Akash Pandya <86837869+Kash-Pandz@users.noreply.github.com>
2025-05-30 09:26:57 +0100

    Update pdb_to_tensor.py

--- pdb_to_tensor.py
+++ pdb_to_tensor.py
@@ -8,12 +8,12 @@ def convert_pdb_to_tensor(pdb_file):
     universe = mda.Universe(pdb_file)
 
     # Select C-alpha atoms and get their positions
-    ca_atoms = u.select_atoms("name CA")
+    ca_atoms = universe.select_atoms("name CA")
     ca_tensor = torch.tensor(ca_atoms.positions, dtype=torch.float32)
     print(ca_tensor.size())
     
     # Select backbone atoms (N, CA, C, O) and get their positions
-    backbone_atoms = u.select_atoms("backbone")
+    backbone_atoms = universe.select_atoms("backbone")
     backbone_tensor = torch.tensor(backbone_atoms.positions, dtype=torch.float32)
     print(backbone_tensor.size())
 
