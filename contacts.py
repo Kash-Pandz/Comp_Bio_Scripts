9631535703277d684839676accf2039a9c6cc667
Akash Pandya <86837869+Kash-Pandz@users.noreply.github.com>
2025-11-06 17:22:54 +0000

    Updated contacts.py

--- contacts.py
+++ contacts.py
@@ -12,11 +12,12 @@ def find_contacts(pdb_file, selection1, selection2, cutoff_dist):
     group2 = universe.select_atoms(selection2)
     
     # Calculate contacts between two atom groups using a cutoff distance
-    contacts = capped_distance(g1, g2, cutoff_dist, return_distances=False)
+    contacts, distances = capped_distance(g1, g2, cutoff=cutoff_dist, return_distances=True)
     left, right = contacts.T
 
     unique_res_pairs = []
     seen = set()
+    
     for i, (l,r) in enumerate(zip(left, right)):
         segid1, resname1, resid1 = g1[l].segid, g1[l].resname, g1[l].resid
         segid2, resname2, resid2 = g2[r].segid, g2[r].resname, g2[r].resid
@@ -27,15 +28,16 @@ def find_contacts(pdb_file, selection1, selection2, cutoff_dist):
             continue
         seen.add(pair_key)
 
-    unique_res_pairs.append({
+        unique_res_pairs.append({
             "segid1": segid1, "resid1": resid1, "resname1": resname1,
             "segid2": segid2, "resid2": resid2, "resname2": resname2,
             "distance": dist
         })
-    
-    
-    return pd.DataFrame(data, columns=[
-        "segid1", "resid1", "resname1",
-        "segid2", "resid2", "resname2",
-        "distance"
+
+    df = pd.DataFrame(unique_res_pairs, columns=[
+         "segid1", "resid1", "resname1",
+         "segid2", "resid2", "resname2",
+         "distance"
     ])
+
+    return df 
